7f10fa0d39659eaf7f8ae2bc6ce4235674f396cf
Josh <zylo.codes@gmail.com>
2022-03-17 20:56:51 -0500

    Clicking and comparing

--- game.js
+++ game.js
@@ -4,14 +4,70 @@ var numTiles = 20
 var startTime = 30
 var time = startTime
 var intervalId
+var score = 0
 
 var countdownEl = document.getElementById('countdown')
 var scoreEl = document.getElementById('score')
 var tilesEl = document.getElementById('tiles')
 
+var flippedTile1 = null
+var flippedTile2 = null
+
 // functions
-function handleTileClick() {
-  console.log('Tile clicked')
+
+function updateScore() {
+  // make background green
+  flippedTile1.classList.add('matched')
+  flippedTile2.classList.add('matched')
+  // increase score
+  score++
+  scoreEl.innerText = score
+  // reset flippedTiles
+  flippedTile1 = null
+  flippedTile2 = null
+}
+
+function unFlipTiles() {
+  // wait 1 second
+  setTimeout(function() {
+    // replace tile's number with brainEmoji
+    flippedTile1.innerText = brainEmoji
+    flippedTile2.innerText = brainEmoji
+    // remove .flipped
+    flippedTile1.classList.remove('flipped')
+    flippedTile2.classList.remove('flipped')
+    // reset flippedTiles
+    flippedTile1 = null
+    flippedTile2 = null
+  }, 1000)
+}
+
+function compareTiles() {
+  var num1 = flippedTile1.getAttribute('data-number')
+  var num2 = flippedTile2.getAttribute('data-number')
+  if (num1 === num2) {
+    updateScore()
+  } else {
+    unFlipTiles()
+  }
+}
+
+function handleTileClick(event) {
+  if (flippedTile1 && flippedTile2) {
+    return
+  }
+  var clickedTile = event.target
+  var num = clickedTile.getAttribute('data-number')
+  clickedTile.innerText = num
+  clickedTile.classList.add('flipped')
+  if (!flippedTile1) {
+    flippedTile1 = clickedTile
+  } else {
+    // assign clicked LI into flippedTile2
+    flippedTile2 = clickedTile
+    // compare flippedTile1's data-number and flippedTile2's data-number
+    compareTiles()
+  }
 }
 
 function createShuffledNumbers() {
