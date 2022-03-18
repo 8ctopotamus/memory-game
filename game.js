8d2dca4bd705164e5ed0eb58705e06c59fa0df92
Josh <zylo.codes@gmail.com>
2022-03-17 20:15:17 -0500

    createShuffledNumbers

--- game.js
+++ game.js
@@ -14,16 +14,26 @@ function handleTileClick() {
   console.log('Tile clicked')
 }
 
+function createShuffledNumbers() {
+  var nums = []
+  for (var i = 0; i < numTiles / 2; i++) {
+    var randNum = Math.floor( Math.random() * 500 )
+    nums.push(randNum, randNum)
+  }
+  nums = nums.sort(function() {
+    return Math.random() - 0.5
+  })
+  return nums
+}
+
 function createTiles() {
-  console.log('Creating tiles...')
-  
-  for (var i = 0; i < numTiles; i++) {
+  var shuffledNums = createShuffledNumbers()
+  for (var i = 0; i < shuffledNums.length; i++) {
     var li = document.createElement('li')
     li.innerText = brainEmoji
-    li.setAttribute('data-number', 2)
+    li.setAttribute('data-number', shuffledNums[i])
     li.classList.add('tile')
     li.addEventListener('click', handleTileClick)
-    console.log(li)
     tilesEl.appendChild(li)
   }
 }
